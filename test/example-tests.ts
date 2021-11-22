1b6208fb18611e4d7e5174a53a6ee9b573f3ddb5
Johan Gorter <johan-gorter@users.noreply.github.com>
2021-11-22 21:55:13 +0100

    after running git/npm/tsa init

---
+++ test/example-tests.ts
@@ -0,0 +1,8 @@
+import { greet } from '../src/example';
+import { expect } from 'chai';
+
+describe('example', () => {
+  it('greets a user by his/her name', () => {
+    expect(greet('Alice')).to.equal('Hello Alice!');
+  });
+});
