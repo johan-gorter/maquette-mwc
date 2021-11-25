9eedd9b697ade97bb4d2b1cc0f59dec0f47d2899
Johan Gorter <johan-gorter@users.noreply.github.com>
2021-11-25 08:34:17 +0100

    drawer

--- src/client/demo.ts
+++ src/client/demo.ts
@@ -1,26 +1,58 @@
 import "@material/mwc-button";
+import "@material/mwc-icon-button";
+import "@material/mwc-top-app-bar-fixed";
+import "@material/mwc-drawer";
 
 import { createProjector, h } from "maquette";
 
 let projector = createProjector();
 
 let toggle = false;
+let drawerOpen = true;
+
 function render() {
-  return h("div", [
-    h(
-      "mwc-button",
-      {
-        raised: true,
-        onclick: () => {
-          toggle = !toggle;
-        },
+  return h("mwc-top-app-bar-fixed", [
+    renderDrawer(),
+    h("mwc-icon-button", {
+      icon: "menu",
+      slot: "navigationIcon",
+      onclick: () => {
+        drawerOpen = !drawerOpen;
       },
-      ["Click me"]
-    ),
-    toggle ? h("p", ["On"]) : h("p", ["Off"]),
+    }),
+    h("div", { slot: "title" }, ["Title"]),
+    h("mwc-icon-button", { icon: "favorite", slot: "actionItems" }),
+    h("div", [
+      h(
+        "mwc-button",
+        {
+          raised: true,
+          onclick: () => {
+            toggle = !toggle;
+          },
+        },
+        ["Click me"]
+      ),
+      toggle ? h("p", ["On"]) : h("p", ["Off"]),
+    ]),
   ]);
 }
 
+function renderDrawer() {
+  return h("mwc-drawer", { type: "modal", open: drawerOpen, afterCreate: afterDrawerCreate }, [
+    h("div", [h("p", ["Drawer content"])]),
+  ]);
+}
+
+function afterDrawerCreate(el: HTMLElement) {
+  el.addEventListener("MDCDrawer:closed", () => {
+    alert("closed");
+  });
+  el.addEventListener("MDCDrawer:opened", () => {
+    alert("opened");
+  });
+}
+
 window.addEventListener("load", () => {
-  projector.replace(document.body, render);
+  projector.replace(document.body, renderDrawer);
 });
